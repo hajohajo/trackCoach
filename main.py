8d114a7d0ac60fd799f5a9d6fdcbecd2dfeeb929
hajohajo <joona.havukainen@helsinki.fi>
2019-04-04 18:10:06 -0700

    Regular WGAN implementation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from keras.engine.input_layer import Input
 from keras import backend as K
 import pandas as pd
 import root_pandas
+from sklearn.preprocessing import StandardScaler
 
 from models import set_trainability, make_generator, make_discriminator, make_gan
 from plotting import plot_losses, plot_distribution
@@ -26,11 +27,14 @@ output_columns = ['trk_dxyClosestPV', 'trk_dzClosestPV', 'trk_ptErr', 'trk_etaEr
 temp = ['trk_isTrue', 'trk_algo']
 input_file = "data/trackingNtuple_TTBarLeptons.root"
 dataframe = root_pandas.read_root(input_file,columns=input_columns+output_columns+temp, flatten=True)[input_columns+output_columns+temp]
-dataframe = dataframe[(dataframe.trk_isTrue==1) & (dataframe.trk_algo==4)]
+dataframe = dataframe[(dataframe.trk_isTrue==1) & (dataframe.trk_algo==4) & (dataframe.trk_pt<=10.0)]
 dataframe = dataframe.drop(temp, axis=1)
 
+scaler = StandardScaler()
+scaler.fit(dataframe[output_columns])
+
 def generate_input(n_samples):
-    pT = np.random.uniform(0.0, 100.0, n_samples)
+    pT = np.random.uniform(0.0, 10.0, n_samples)
     eta = np.random.uniform(-2.1, 2.1, n_samples)
     phi = np.random.uniform(-math.pi, math.pi, n_samples)
 
@@ -40,6 +44,7 @@ def generate_input(n_samples):
 def sample_data_and_gen(G, n_samples):
     inp = generate_input(n_samples/2)
     generated = G.predict(inp)
+    generated = scaler.inverse_transform(generated)
     generated = np.concatenate((inp, generated), axis=1)
     real = dataframe.sample(n=n_samples/2)
     output = pd.DataFrame(np.concatenate((generated, real), axis=0), columns=input_columns+output_columns)
@@ -53,9 +58,10 @@ def sample_data_and_gen(G, n_samples):
 def pretrain(G, D, n_samples, batch_size=32, epochs=10):
     X, y = sample_data_and_gen(G, n_samples)
     set_trainability(D, True)
-    D.fit(X[output_columns], y, epochs=epochs, batch_size=batch_size, verbose=False)
+#    D.fit(X[output_columns], y, epochs=epochs, batch_size=batch_size, verbose=False)
+    D.fit(scaler.transform(X[output_columns]), y, epochs=epochs, batch_size=batch_size, verbose=False)
 
-def train(GAN, G, D, epochs=100, n_samples=20000, batch_size=64, verbose=False, v_freq=10):
+def train(GAN, G, D, epochs=100, n_samples=50000, batch_size=64, verbose=False, v_freq=10):
     d_iters=10
     D_loss = []
     G_loss = []
@@ -67,7 +73,8 @@ def train(GAN, G, D, epochs=100, n_samples=20000, batch_size=64, verbose=False, 
         for batch in range(n_samples/batch_size):
             X, y = sample_data_and_gen(G, batch_size)
             set_trainability(D, True)
-            d_loss.append(D.train_on_batch(X[output_columns], y))
+#            d_loss.append(D.train_on_batch(X[output_columns], y))
+            d_loss.append(D.train_on_batch(scaler.transform(X[output_columns]), y))
 
             set_trainability(D, False)
             X = generate_input(batch_size)
@@ -85,14 +92,11 @@ def train(GAN, G, D, epochs=100, n_samples=20000, batch_size=64, verbose=False, 
                 plot_distribution(X[y==1][distr], binning, epoch=epoch+1, title="Generated_"+distr)
                 plot_distribution(X[y==-1][distr], binning, epoch=epoch+1, title="Real_"+distr)
 
-        if (epoch + 1) % 200 == 0:
-            print "Old lr: "+ str(K.eval(D.optimizer.lr))
-            K.set_value(D.optimizer.lr, 0.5*K.eval(D.optimizer.lr))
-            K.set_value(G.optimizer.lr, 0.5*K.eval(G.optimizer.lr))
-            print "New lr: "+ str(K.eval(D.optimizer.lr))
-
-#            D.optimizer.lr.set_value(0.1*D.optimizer.lr.get_value())
-#            G.optimizer.lr.set_value(0.1*G.optimizer.lr.get_value())
+        # if (epoch + 1) % 200 == 0:
+        #     print "Old lr: "+ str(K.eval(D.optimizer.lr))
+        #     K.set_value(D.optimizer.lr, 0.5*K.eval(D.optimizer.lr))
+        #     K.set_value(G.optimizer.lr, 0.5*K.eval(G.optimizer.lr))
+        #     print "New lr: "+ str(K.eval(D.optimizer.lr))
 
     return D_loss, G_loss
 
--- models.py
+++ models.py
@@ -4,11 +4,10 @@ from keras.engine.topology import  Layer
 from keras.models import Model
 from keras.layers import Input
 from keras.layers.core import Dense
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSprop
 from keras.callbacks import TensorBoard
 from keras.regularizers import l2
 from keras.initializers import RandomNormal
-
 
 regrate = 1e-7
 
@@ -58,13 +57,17 @@ def make_generator(G_in, out_dim, lr=1e-3):
     x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
     x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
     x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
+    x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
+    x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
+    x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
+    x = Dense(50, activation=swish, kernel_initializer='he_uniform', kernel_regularizer=l2(regrate))(x)
     G_out = Dense(out_dim, activation='linear', kernel_initializer='he_uniform')(x)
     G = Model(G_in, G_out)
-    G.compile(loss='mse', optimizer=Adam(lr, amsgrad=True))
+    G.compile(loss='mae', optimizer=RMSprop(lr))
     return G, G_out
 
 
-weight_init = RandomNormal(mean=0., stddev=0.2)
+weight_init = RandomNormal(mean=0., stddev=0.02)
 
 from keras.constraints import Constraint
 class WeightClip(Constraint):
@@ -80,19 +83,15 @@ class WeightClip(Constraint):
         return {'name': self.__class__.__name__,
                 'c': self.c}
 
-clipvalue=0.1
+clipvalue=0.01
 ###Discriminator model
 def make_discriminator(D_in, lr=1e-3):
     x = Dense(20, activation=swish, kernel_initializer=weight_init, kernel_regularizer=l2(regrate), W_constraint = WeightClip(clipvalue))(D_in)
     x = Dense(20, activation=swish, kernel_initializer=weight_init, kernel_regularizer=l2(regrate), W_constraint = WeightClip(clipvalue))(x)
     x = Dense(20, activation=swish, kernel_initializer=weight_init, kernel_regularizer=l2(regrate), W_constraint = WeightClip(clipvalue))(x)
-    x = Dense(20, activation=swish, kernel_initializer=weight_init, kernel_regularizer=l2(regrate), W_constraint = WeightClip(clipvalue))(x)
-    x = Dense(20, activation=swish, kernel_initializer=weight_init, kernel_regularizer=l2(regrate), W_constraint = WeightClip(clipvalue))(x)
-    D_out = Dense(1, activation='linear', kernel_initializer=weight_init, W_constraint = WeightClip(0.01))(x)
+    D_out = Dense(1, activation='linear', kernel_initializer=weight_init, W_constraint = WeightClip(clipvalue))(x)
     D = Model(D_in, D_out)
-#    D.compile(loss='binary_crossentropy', optimizer=Adam(lr, amsgrad=True))
-#    D.compile(loss='mse', optimizer=Adam(lr, amsgrad=True))
-    D.compile(loss=multiply_loss, optimizer=Adam(lr, amsgrad=True))
+    D.compile(loss=multiply_loss, optimizer=RMSprop(lr))
     return D, D_out
 
 ###The chained model i.e. GAN
@@ -101,7 +100,6 @@ def make_gan(GAN_in, G, D):
     x = G(GAN_in)
     GAN_out = D(x)
     GAN = Model(GAN_in, GAN_out)
-#    GAN.compile(loss='mse', optimizer=G.optimizer)
     GAN.compile(loss=multiply_loss, optimizer=G.optimizer)
     return GAN, GAN_out
 
